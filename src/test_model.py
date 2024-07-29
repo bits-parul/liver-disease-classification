74db7145d94ac98df660ce21e90bad4f5eff5d16
Parul Arora <parularo@basware.com>
2024-07-29 19:30:24 +0530

    Updated files again.

--- src/test_model.py
+++ src/test_model.py
@@ -2,10 +2,12 @@ import unittest
 from model_util import load_model, predict
 import pandas as pd
 
+
 class TestModel(unittest.TestCase):
     def test_model_loading(self):
         model = load_model('models/liver_disease_randomforest_model.joblib')
         self.assertIsNotNone(model)
+
 
     def test_model_prediction(self):
         model = load_model('models/liver_disease_randomforest_model.joblib')
