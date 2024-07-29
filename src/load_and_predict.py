72c11a552b3a0a74435295ca102c07622250302b
Parul Arora <parularo@basware.com>
2024-07-29 19:24:44 +0530

    Updated the files.

--- src/load_and_predict.py
+++ src/load_and_predict.py
@@ -7,19 +7,19 @@ if __name__ == "__main__":
     
     # Example new data for prediction
     new_data = pd.DataFrame({
-        'Age': [45],
-        'Total_Bilirubin': [1.0],
-        'Direct_Bilirubin': [0.3],
-        'Alkaline_Phosphotase': [200],
-        'Alamine_Aminotransferase': [50],
-        'Aspartate_Aminotransferase': [45],
-        'Total_Protiens': [6.5],
-        'Albumin': [3.5],
-        'Albumin_and_Globulin_Ratio': [1.0]
+        'Age': [74.000000],
+        'Total_Bilirubin': [0.900000],
+        'Direct_Bilirubin': [0.300000],
+        'Total_Protiens': [7.900000],
+        'Albumin': [4.000000],
+        'Albumin_and_Globulin_Ratio': [1.000000],
+        'Alkaline_Phosphotase_normalized': [3.490333],
+        'Alamine_Aminotransferase_normalized': [1.421247],
+        'Aspartate_Aminotransferase_normalized': [1.574394]
     })
 
     # Make predictions
     predictions = predict(model, new_data)
-    decoded_predictions = ["Yes" if pred==1 else "No" for pred in predictions]
+    decoded_predictions = ["Yes" if pred == 1 else "No" for pred in predictions]
     print("Predictions:", decoded_predictions)
     
--- src/model_util.py
+++ src/model_util.py
@@ -1,11 +1,10 @@
 # Importing the libraries.
 
 import joblib
-import pandas as pd
 
 
 def load_model(model_path):
-    return jiblib.load_model(model_path)
+    return joblib.load(model_path)
     
     
 def predict(model, data):
