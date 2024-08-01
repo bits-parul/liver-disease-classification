d1759af7b11bdf6d91ae14e8f2db9c12be752575
Parul Arora <parularo@basware.com>
2024-08-01 22:10:17 +0530

    Modified the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,13 @@ REMOTE_HOST="16.171.44.242"
 REMOTE_PATH="/home/ec2-user"
 MODEL_PATH="/home/ec2-user/model-deployment/models"
 APP_PATH="/home/ec2-user/model-deployment/app"
-KEY_PATH="/home/ec2-user/testing-key.pem"
+KEY_PATH="/home/ec2-user/liver-server-key.pem"
 
 # Copy the model file to the EC2 instance
 scp -i "$KEY_PATH" "models/liver_disease_randomforest_model.joblib" $REMOTE_USER@$REMOTE_HOST:$MODEL_PATH
 
 # Copy the Flask application files to the EC2 instance
-scp -i "$KEY_PATH" "src/app.py" $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
+scp -i "$KEY_PATH" "src/app.py" $REMOTE_USER@$REMOTE_HOST:$APP_PATH
 
 # SSH into the EC2 instance and start the Flask application
 ssh -i "$KEY_PATH" $REMOTE_USER@$REMOTE_HOST << 'EOF'
